0bb1047af3e0113d311d8be38579547f7714d3be
Affan <affanraze@gmail.com>
2026-05-12 16:39:02 +0500

    utils code added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import "dotenv/config";
 import dbConnection from "./db/index.js";
-
+import app from "./app.js";
 dbConnection()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Errr while listening", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is listening on ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("Mongo Db connection failed", err));
