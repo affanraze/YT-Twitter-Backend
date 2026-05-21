b7b166f3b10949e9fe64cb40873ce27032145613
affan <affanraze@gmail.com>
2026-05-21 13:42:50 +0530

    login user controller added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -42,7 +42,7 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Password is required"],
     },
-    refeshToken: {
+    refreshToken: {
       type: String,
     },
   },
